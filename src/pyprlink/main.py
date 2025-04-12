59545b89dc6def341ed239e462f0b57c31ed8797
JenuC <jenuvchacko@gmail.com>
2025-04-12 03:40:03 -0500

    versioning sync  under toml using importlib

--- src/pyprlink/main.py
+++ src/pyprlink/main.py
@@ -1,11 +1,12 @@
 from pyprlink.tcp_client import ask_PV
+from pyprlink import __version__
 import sys
 import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='PyPrLink - TCP/IP client for communicating with PrairieView')
     parser.add_argument('command', nargs='+', help='Command and arguments to send to PrairieView')
-    parser.add_argument('--version', action='version', version='%(prog)s 0.1.0')
+    parser.add_argument('--version', action='version', version=f'%(prog)s {__version__}')
     
     args = parser.parse_args()
     
