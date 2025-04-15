cbccbc23b09cb547aa730972b3da3ad039d59825
JenuC <jenuvchacko@gmail.com>
2025-04-14 19:24:13 -0500

    basic arg parser broke. this unknown arg passing will move commands without positions or definitions.

--- src/pyprlink/main.py
+++ src/pyprlink/main.py
@@ -4,17 +4,55 @@ import sys
 import argparse
 
 def main():
-    parser = argparse.ArgumentParser(description='PyPrLink - TCP/IP client for communicating with PrairieView')
-    parser.add_argument('command', nargs='+', help='Command and arguments to send to PrairieView')
-    parser.add_argument('--version', action='version', version=f'%(prog)s {__version__}')
-    
-    args = parser.parse_args()
-    
-    try:
-        ask_PV(*args.command)
-    except Exception as e:
-        print(f"Error: {e}", file=sys.stderr)
-        sys.exit(1)
+
+    crude_default = False
+
+    if not crude_default:
+        
+        parser = argparse.ArgumentParser(description='PyPrLink - TCP/IP client for communicating with PrairieView')
+        parser.add_argument('--version', action='version', version=f'%(prog)s {__version__}')  
+        #parser.add_argument('-gmp', choices=['x','y','z'], help='get motor position for motor') 
+        #parser.add_argument('-pg', nargs=2, help='set PMT Gain value') 
+        
+        #args = parser.parse_args()
+        #args,message = parser.parse_known_args()
+        #print(message)
+        args,message = parser.parse_known_intermixed_args()
+        print(args, message)
+        
+        #    if args.gmp is not None:
+        #        message = ["-gmp"]+ [args.gmp]
+        #    if args.pg is not None:
+        #        message = ["-pg"]+ args.pg
+        
+        #print(message, *message)
+        try:
+            print(*message)
+            ask_PV(*message)
+        except Exception as e:
+            print(f"Error: {e}", file=sys.stderr)
+            sys.exit(1)
+
+        
+        
+        
+    if crude_default:
+        args = sys.argv[1:]
+        
+        if '--version' in args:
+            args.remove('--version')
+            print(f'{__version__}')  
+            
+        elif '--help' in args:
+            args.remove('--help')
+            print(f'cmd {args}')  
+            
+        else:
+            try:
+                ask_PV(*args)
+            except Exception as e:
+                print(f"Error: {e}", file=sys.stderr)
+                sys.exit(1)
 
 if __name__ == "__main__":
     main()        
